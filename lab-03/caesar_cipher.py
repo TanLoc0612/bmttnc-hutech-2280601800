f6bf56d56e1b6cedf6065dbbfa9598da947440ba
Tan Loc <daotanloc@gmail.com>
2025-06-11 10:47:51 +0700

    [add] lab-03 ui caesar

--- lab-03/caesar_cipher.py
+++ lab-03/caesar_cipher.py
@@ -8,21 +8,20 @@ class MyApp(QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.btn_encrypt.clicked.connect(self.call_api_encrypt)
-        self.ui.btn_decrypt.clicked.connect(self.call_api_decrypt)
+        self.ui.btnEn.clicked.connect(self.call_api_encrypt)
+        self.ui.btnDe.clicked.connect(self.call_api_decrypt)
 
     def call_api_encrypt(self):
-        url = "http://127.0.0.1:5000/api/caesar/encrypt"
+        url = "http://127.0.0.1:8080/api/caesar/encrypt"
         payload = {
-            "plain_text": self.ui.txt_plain_text.toPlainText(),
-            "key": self.ui.txt_key.toPlainText()  # Sửa từ .text() thành .toPlainText()
+            "plain_text": self.ui.txtPlain.toPlainText(),
+            "key": self.ui.txtKey.toPlainText()  # Sửa từ .text() thành .toPlainText()
         }
         try:
             response = requests.post(url, json=payload)
             if response.status_code == 200:
                 data = response.json()
-                self.ui.txt_cipher_text.setPlainText(data["encrypted_message"])
-
+                self.ui.txtCipher.toPlainText(data["encrypted_text"])
                 
                 msg = QMessageBox()
                 msg.setIcon(QMessageBox.Information)
@@ -34,16 +33,16 @@ class MyApp(QMainWindow):
             print("Error: %s" % str(e))  # Sửa e.message thành str(e) vì e.message đã bị deprecated
 
     def call_api_decrypt(self):
-        url = "http://127.0.0.1:5000/api/caesar/decrypt"
+        url = "http://127.0.0.1:8080/api/caesar/decrypt"
         payload = {
-            "cipher_text": self.ui.txt_cipher_text.toPlainText(),
-            "key": self.ui.txt_key.toPlainText()  # Sửa từ .text() thành .toPlainText()
+            "cipher_text": self.ui.txtCipher.toPlainText(),
+            "key": self.ui.txtKey.toPlainText()  # Sửa từ .text() thành .toPlainText()
         }
         try:
             response = requests.post(url, json=payload)
             if response.status_code == 200:
                 data = response.json()
-                self.ui.txt_plain_text.setText(data["decrypted_message"])
+                self.ui.txtPlain.setText(data["decrypted_message"])
 
                 msg = QMessageBox()
                 msg.setIcon(QMessageBox.Information)
